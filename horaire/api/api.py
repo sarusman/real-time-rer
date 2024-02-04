eb23d3e3762e2882c4e980e966d57dd5a34dfffc
Sarusman <sarusman.satkunarajah1@gmail.com>
2024-02-04 15:44:20 +0100

    Update api.py

--- horaire/api/api.py
+++ horaire/api/api.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 #URL de l'API Prochains Passages de source IDFM - requête unitaire
 url = "https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopArea%3ASP%3A43122%3A&LineRef=STIP%3ALine%3A%3AC01743%3A"
 #Le header doit contenir la clé API : apikey, veuillez remplacer #VOTRE CLE API par votre clé API
-headers = {"Accept": "application/json","apikey": "rrQPwOvQUnqinvU1De0sgLnl9NDmo2uj"}
+headers = {"Accept": "application/json","apikey": "ICI"}
 
 def get_horaire(type, num, stop):
 	req = requests.get(url, headers=headers)
